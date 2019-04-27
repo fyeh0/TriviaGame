26b3cccae5194276c53e80f748a0395333a5c049
fyeh0 <fangiyeh@gmail.com>
2019-04-27 12:13:56 -0400

    html/css, questions added

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// start/finish button functions
+$("#start-game").click(function () {
+    console.log("Start game");
+})
+
+$("#finish-game").click(function () {
+    console.log("Finish game");
+})
+
+
+// variables
+
+// countdown timer
+
+// add and display correct and incorrect answers
